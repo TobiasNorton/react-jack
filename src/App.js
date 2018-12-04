e4e9fb5df931fe4dec593daceaa6a12e443eb945
Tobias Norton <tnorton20@yahoo.com>
2018-12-04 11:06:31 -0500

    Hand component in place

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import update from 'immutability-helper'
+import Hand from './Hand'
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +9,8 @@ class App extends Component {
 
     this.state = {
       deck_id: '',
-      player: []
+      player: [],
+      dealer: []
     }
   }
 
@@ -17,10 +19,17 @@ class App extends Component {
     axios
       .get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=2`)
       .then(response => {
-        console.log(response.data.cards)
-
         const newState = {
           player: update(this.state.player, { $push: response.data.cards })
+        }
+        this.setState(newState)
+      })
+
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=2`)
+      .then(response => {
+        const newState = {
+          dealer: update(this.state.dealer, { $push: response.data.cards })
         }
         this.setState(newState)
       })
@@ -52,16 +61,18 @@ class App extends Component {
             <button className="hit">Hit</button>
             <p>Your Cards:</p>
             <p className="player-total">Total 0</p>
-            <div className="player-hand" />
+            <div className="player-hand">
+              <Hand cards={this.state.player} />
+            </div>
           </div>
-
           <div className="right">
             <button className="stay">Stay</button>
             <p>Dealer Cards:</p>
             <p className="dealer-total">Facedown</p>
             <div className="dealer-hand">
-              <img className="cardback-one" alt="card" src="./images/card back red.png" />
-              <img className="cardback-two" alt="card" src="./images/card back red.png" />
+              <Hand cards={this.state.dealer} />
+              {/* <img className="cardback-one" alt="card" src="./images/card back red.png" />
+              <img className="cardback-two" alt="card" src="./images/card back red.png" /> */}
             </div>
           </div>
         </div>
